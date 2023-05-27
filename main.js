ad4d97fd401c6680ecce957c7dcc805158bbe932
lijiaxin <www2421800049@163.com>
2023-05-27 12:37:16 +0800

    删除跨域配置，在nginx中配置

--- main.js
+++ main.js
@@ -17,17 +17,17 @@ function main() {
     app.use(express.urlencoded({ extended: true })); // 允许解析 URL 编码的请求体
     app.use(express.json()); // 允许解析 JSON 请求体
 
-    app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*'); // 允许来自任何来源的跨域请求
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // 允许特定的请求头
-        next();
-      });
+    // app.use((req, res, next) => {
+    //     res.header('Access-Control-Allow-Origin', '*'); // 允许来自任何来源的跨域请求
+    //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // 允许特定的请求头
+    //     next();
+    //   });
       
 
     app.post('/v1/translator', async(req, res) => {
         var { word, token } = req.body; // 从请求体中获取 word 和 token 参数
         word = decodeURIComponent(word)
-        res.header('Access-Control-Allow-Origin', '*'); // 允许跨域访问
+        // res.header('Access-Control-Allow-Origin', '*'); // 允许跨域访问
       
         if (token === 'lkymnCgQjaJnCyjh') {
           const result = await bdt.main(word);
